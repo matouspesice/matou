c63728c561d6e1616ff46c0ebdf28e1b533d58e4
matouspesice <matous.pesice@gmail.com>
2026-01-09 18:09:55 +0100

    Add remaining config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: { extend: {} },
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}", // This line tells Tailwind to scan your React files
+    ],
+    theme: {
+      extend: {},
+    },
     plugins: [],
   }
